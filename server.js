8d4925947697d1dabd9adfdcd6b7f204c4088a61
Srineel <srineelx23@gmail.com>
2025-06-16 23:42:37 +0530

    Fix CORS issue for Netlify deployment 4

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(exp.static(path.join(__dirname,'../client/build')))
 app.use(exp.json())
 
 app.use(cors({
-  origin: 'https://aquamarine-stardust-6b2017.netlify.app',
+  origin: 'https://dytbyronin.netlify.app/',
   credentials: true
 }));
 
